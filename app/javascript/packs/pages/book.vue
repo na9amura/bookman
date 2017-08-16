1ce3f45a06dd68992b07bcff67168e359beee723
Nakamura Takahiro <takahiro.nakamura@gi-no.jp>
2017-08-16 10:22:53 +0900

    change layout of show book page

--- app/javascript/packs/pages/book.vue
+++ app/javascript/packs/pages/book.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
     <div v-if="book !== undefined && book.length != 0">
-      <p>{{ book.id }}: {{ book.name }}</p>
+      <div class="titles">
+        <h2>{{ book.title }}</h2>
+        <h3>著者：{{ book.author }}</h3>
+      </div>
+      <div class="information">
+        <div class="image"></div>
+        <div class="text">
+          <div class="text--title">
+            ISBN
+          </div>
+          <div class="text--content">
+            {{ book.isbn }}
+          </div>
+        </div>
+      </div>
+      <div class="checkouts">
+        <button type="button" v-on:click="checkout"></button>
+      </div>
     </div>
   </div>
 </template>
@@ -36,3 +53,22 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.information {
+  display: flex;
+}
+
+.image {
+  flex-shrink: 6;
+  background-color: gray;
+  width: 100%;
+}
+
+.text {
+  flex-shrink: 3;
+  width: 100%;
+  padding: 2%;
+}
+
+</style>
