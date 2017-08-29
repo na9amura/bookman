61407fba7fd1ffbd6ca00012328120ba660e8638
Nakamura Takahiro <takahiro.nakamura@gi-no.jp>
2017-08-29 23:21:50 +0900

    lint

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import Vue from 'vue/dist/vue.esm'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
@@ -11,31 +11,4 @@ document.addEventListener('DOMContentLoaded', () => {
     template: '<App/>',
     components: { App }
   })
-})
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   Vue.use(VueRouter)
-//
-//   const router = new VueRouter({
-//     mode: 'history',
-//     base: __dirname,
-//     routes: routes
-//   })
-//
-//   new Vue({
-//     router,
-//     render: h => h(App),
-//   }).$mount('#app')
-//
-//   // const app = new Vue({
-//   //   el: '#app',
-//   //   data: {
-//   //     searchQuery: '',
-//   //   },
-//   //   components: {
-//   //     App,
-//   //     Hello,
-//   //     Books,
-//   //   },
-//   // })
-// })
+});
