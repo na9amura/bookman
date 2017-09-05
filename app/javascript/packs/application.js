57bf1a43fb31d0dff961017dd6b8804ace6acf7d
Nakamura Takahiro <dxbxdxbxdxb+dev@gmail.com>
2017-09-06 01:59:39 +0900

    add sanitize.css, materialize

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,12 @@
 import Vue from 'vue/dist/vue.esm'
 import App from './App'
 import router from './router'
+import VueMaterial from 'vue-material/dist/vue-material'
 
 Vue.config.productionTip = false;
 
 document.addEventListener('DOMContentLoaded', () => {
+  Vue.use(VueMaterial)
   new Vue({
     el: '#app',
     router,
