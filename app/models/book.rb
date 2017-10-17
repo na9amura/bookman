e2cb20e2b71a739b386d70adc56db612b6c939c6
Nakamura Takahiro <dxbxdxbxdxb+dev@gmail.com>
2017-10-17 10:49:50 +0900

    save publisher and show its name

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,14 +1,18 @@
 class Book < ApplicationRecord
   include ImageUploader::Attachment.new(:image)
   belongs_to :shelf, optional: true
+  belongs_to :publisher
   has_many :tag_assigns
   has_many :tags, through: :tag_assigns
   has_many :checks
   has_one :checking_out, -> { Check.out }, class_name: 'Check'
 
+  attr_accessor :publisher_name
+
   def self.import(attributes)
-    book = Book.new(attributes.reject { |k| k.to_sym == :image_url })
+    book = Book.new(attributes.reject { |k| k.to_sym.in? %i(image_url publisher_name) })
     book.image = open(attributes[:image_url])
+    book.publisher = Publisher.find_or_create_by(name: attributes[:publisher_name])
     book
   end
 
