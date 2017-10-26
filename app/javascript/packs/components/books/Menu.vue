4ec18d23d550f8faccd55961ae9e634118151708
Nakamura Takahiro <dxbxdxbxdxb+dev@gmail.com>
2017-10-26 11:03:22 +0900

    fix to move menu

--- app/javascript/packs/components/books/Menu.vue
+++ app/javascript/packs/components/books/Menu.vue
@@ -2,20 +2,20 @@
   <div>
     <md-button
       class="md-raised md-primary"
-      @click.stop="showShelves(book)"
-      @click.right="showShelves(book)">
+      @click.stop="showShelves"
+      @click.right="showShelves">
       本棚変更
     </md-button>
-    <md-menu :ref="shelfMenuRef(book)">
+    <md-menu ref="menu">
       <span md-menu-trigger></span>
       <md-menu-content>
         <md-menu-item
           v-if="shelves.length"
           v-for="shelf in shelves"
-          @click="assignShelf(index, book, shelf)"
+          @click="assignShelf(index, shelf)"
           :key="shelf.id"
-          :disabled="!selectableShelf(book, shelf)">
-          {{ selectableShelf(book, shelf) ? '' : '* ' }}
+          :disabled="!selectableShelf(shelf)">
+          {{ selectableShelf(shelf) ? '' : '* ' }}
           {{ shelf.name }}
         </md-menu-item>
       </md-menu-content>
@@ -24,6 +24,8 @@
 </template>
 
 <script>
+  import Books from '../../models/global/books'
+
   export default {
     name: 'book-menu',
     props: {
@@ -31,26 +33,31 @@
       book: Object,
       shelves: Array,
     },
+    data() {
+      return {
+        books: Books,
+      }
+    },
     methods: {
-      shelfMenuRef(book) {
-        return `shelfMenu${ book.id }`
+      shelfMenuRef() {
+        return `shelfMenu${ this.book.id }`
       },
-      showShelves(book) {
-        this.$refs[this.shelfMenuRef(book)][0].toggle()
+      showShelves() {
+        this.$refs.menu.toggle()
       },
-      assignShelf(index, book, shelf) {
+      assignShelf(index, shelf) {
         const vm = this
         axios
           .patch(
-            `/books/${ book.id }.json`,
+            `/books/${ this.book.id }.json`,
             { book: { shelf_id: shelf.id } }
           )
           .then((response) => {
             vm.books.state.list.splice(index, 1, response.data)
           })
       },
-      selectableShelf(book, newShelf) {
-        return book.shelf_id !== newShelf.id
+      selectableShelf(newShelf) {
+        return this.book.shelf_id !== newShelf.id
       },
     }
   }
