1a0dff622b9c015d8b7d97b8d1bada495b2ac9ff
Nakamura Takahiro <dxbxdxbxdxb+dev@gmail.com>
2017-11-07 11:04:26 +0900

    set up edit page and route to there

---
+++ app/javascript/packs/pages/EditBook.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>
+      Edit {{ book.title }}
+    </h2>
+    <md-input-container>
+      <label>title</label>
+      <md-input v-model="book.title"></md-input>
+    </md-input-container>
+    <md-input-container>
+      <label>publisher_name</label>
+      <md-input v-model="book.publisher_name"></md-input>
+    </md-input-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'edit-book',
+    props: {
+      id: Number
+    },
+    data() {
+      return {
+        book: {},
+      }
+    },
+    created() {
+      this.init()
+    },
+    methods: {
+      init() {
+        this.loadBook()
+      },
+      loadBook() {
+        let vm = this
+        axios.get(`/books/${ vm.id }.json`)
+          .then((response) => { vm.book = response.data })
+      },
+    },
+  }
+</script>
+
+<style scoped lang="sass">
+</style>
